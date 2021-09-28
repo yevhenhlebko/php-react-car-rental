cca86bf1976d436779af2aaf17c71e679f5118ac
yevhenhlobko <yevhenhlobko@gamil.com>
2021-09-28 05:22:24 -0700

    AJE-3 new update

--- resources/js/components/footer.js
+++ resources/js/components/footer.js
@@ -5,7 +5,7 @@ function Footer () {
     <div className="bg-gray-100 h-32 text-center">
       <div className="container mx-auto flex flex-col">
         <p className="px-4 py-8 text-gray-700 font-light text-sm">
-        &copy;{(new Date()).getFullYear()}  Laravel React SPA
+        &copy;{(new Date()).getFullYear()}  AJ'S EXPERIENCE
         </p>
       </div>
     </div>
