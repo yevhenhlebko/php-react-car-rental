887e9f26469b414afb360a55bca46d2607a82333
yevhenhlobko <yevhenhlobko@gamil.com>
2021-09-30 14:55:12 -0700

    AJE-16-update-user-management

---
+++ resources/js/pages/user-approval.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+
+import { getUsers } from '../api/auth';
+import { setUserAction } from '../api/user-approve';
+import { Link } from 'react-router-dom';
+
+function UserApproval () {
+  const [actionChanged, setActionChanged] = useState(false);
+  const [currentUsers, setCurrentUsers] = useState([]);
+
+  const userAction = (action, id) => {
+    setUserAction({
+      action: action,
+      id: id
+    }).then(status => {
+      setActionChanged(!actionChanged);
+    // eslint-disable-next-line handle-callback-err
+    }).catch(error => {
+    });
+  };
+  useEffect(() => {
+    getUsers().then((users) => {
+      console.log('userss', users);
+      setCurrentUsers(users);
+    });
+  }, [actionChanged]);
+
+  return (
+    <div className="flex justify-center items-center w-full flex-col py-4 min-h-screen bg-black">
+
+      <div className="p-8 flex flex-col items-center">
+        <div className="ajs-header text-center text-6xl leading-loose text-white font-bungee font-bold">
+                    AJ’s Experience
+        </div>
+      </div>
+
+      <div className="box-border overflow-hidden text-2xl form-box-shadow mix-blend-normal rounded-3xl border-grey-light w-3/4 sm:w-1/2 lg:w-3/5 xl:w-3/5 px-8 py-4 bg-black">
+        <div className="mb-4 mt-4">
+          <div className="card-body bg-black text-white appearance-none  font-inter rounded w-full py-1 px-3">
+            <table>
+              <thead>
+                <tr className="w-full text-center">
+                  <th>ID</th>
+                  <th className='w-1/5'>Name</th>
+                  <th className='w-2/5'>Email</th>
+                  <th>Status</th>
+                  <th className='w-1/5'>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  currentUsers
+                    ? currentUsers.map((user, index) => (
+                      <tr key={index} className="w-full text-center" >
+                        <td>{user.id}</td>
+                        <td className='w-1/5'>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.ready_review == '1' ? 'Accepted' : 'Rejected'}</td>
+                        <td>
+                          <div className="flex">
+                            <p className="flex flex-col px-4 py-4 m-auto"
+                              onClick={() => userAction('accept', user.id)}
+                            >
+                              <Link
+                                to="#"
+                                className="no-underline text-white font-julius font-normal text-opacity-75">
+                                                                Accept</Link>
+                            </p>
+                            <p className="flex flex-col px-4 py-4 m-auto"
+                              onClick={() => userAction('reject', user.id)}
+                            >
+                              <Link
+                                to="#"
+                                className="no-underline text-white font-julius font-normal text-opacity-75">
+                                                                Reject</Link>
+                            </p>
+                          </div>
+                        </td>
+                      </tr>
+                    )) : 'Loading...'
+                }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserApproval;
